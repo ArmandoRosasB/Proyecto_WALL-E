6841cef3b5379f4701d4876388efffba5c1c6c8d
RamonaNF <a01423596@tec.mx>
2023-08-18 12:46:26 -0600

    feat: Agent movements v1

--- MultiCleaningSystem/Agent/Agent.py
+++ MultiCleaningSystem/Agent/Agent.py
@@ -1,23 +1,46 @@
 # Paquetes para trabajar con agentes
 from mesa import Agent, Model
-from mesa.model import Model
 
 class Scavenger(Agent):
+    """ Clase heredada de mesa.agent que representa a un robot """
+    
     def __init__(self, id: int, model: Model) -> None:
+        """ El constructor recibe como parámetros el id del agente y el modelo 
+        sobre le cual opera """
         super().__init__(id, model)
 
         self.value = 'R'
         self.storage = 5
+
+    def step(self):
+        """ Este metodo mueve a los robots a la posición más óptima """
+        neighbors = self.model.grid.get_neighbors(self.pos, moore = True, include_center = False)  # Regresa un vector
+
+        movements = []
+        for agent in neighbors:
+            if agent.value == 'T':
+                movements.append(agent.pos)
+
+        if len(movements) != 0: # Buscar mejor posibilidad
+            self.pos = movements[0]
     
 class Trash(Agent):
+    """ Clase heredada de mesa.agent que representa una pila de basura """
+
     def __init__(self, id: int, model: Model) -> None:
+        """ El constructor recibe como parámetros el id del agente y el modelo 
+        sobre le cual opera """
         super().__init__(id, model)
 
         self.value = 'T'
         self.detritus = 0
 
 class Wall (Agent):
+    """ Clase heredada de mesa.agent que representa una pared u obstaculo"""
+
     def __init__(self, id: int, model: Model) -> None:
+        """ El constructor recibe como parámetros el id del agente y el modelo 
+        sobre le cual opera """
         super().__init__(id, model)
         
         self.value = 'X'
